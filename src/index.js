ac5f1a48fb5e55b49c9e18100ee834c42c618bc5
Денис Ростокин <denis.rostokin@osinit.com>
2018-09-28 17:02:22 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
+
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+class appState {
+  @observable count = 0
+
+  increment = () => this.count++
+
+  decrement = () => this.count--
+}
+
+@observer
+class Count extends Component {
+  handleInc = () => this.props.store.increment()
+
+  handleDec = () => this.props.store.decrement()
+
+  render() {
+    const { count } = this.props.store
+
+    return (
+      <div>
+        <DevTools />
+        Counter: {count}<br />
+        <button onClick={this.handleDec}>-</button>
+        <button onClick={this.handleInc}>+</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Count store={new appState()} />, document.getElementById('root'));
+registerServiceWorker();
